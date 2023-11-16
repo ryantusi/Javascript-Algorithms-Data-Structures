e3059b2926977f4408898e7a1e22c9ceb5f4e05c
Ryan Tusi <ryantusi45@gmail.com>
2023-11-16 21:48:26 +0530

    Uploaded

---
+++ Palindrome/palindrome.js
@@ -0,0 +1,22 @@
+// A word is palindrome if the reverse is same as the original
+
+// Using Data Structures and Algorithm, building our own palindrome checker without using built-in functions
+
+function palindrome(str) {
+    let word = str.toLowerCase();
+    let reverse = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverse += word[i];
+    }
+    console.log("Original Word: " + word);
+    console.log("Reversed: " + reverse);
+    if (word == reverse) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+// Test it out by replacing the string inside palindrome()
+const result = palindrome("Mom");
+console.log("Is Palindrome? " + result )
